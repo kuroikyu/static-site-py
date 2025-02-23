904e7c40d39c7e5eb6a83f095790d54cc67839c0
Kuroi Kyu <hello@kuroikyu.com>
2025-02-23 23:51:50 +0100

    feat: add block type detection

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str):
     blocks = markdown.split("\n\n")
     result = []
@@ -6,3 +19,43 @@ def markdown_to_blocks(markdown: str):
         if len(block) > 0:
             result.append(block)
     return result
+
+
+def block_to_block_type(block: str):
+    lines = block.split("\n")
+    first_line = lines[0]
+    last_line = lines[-1]
+
+    # Heading
+    if re.search(r"^#{1,6}\s", first_line):
+        return BlockType.HEADING
+
+    # Code
+    if re.search(r"^`{3}", first_line) and re.search(r"`{3}$", last_line):
+        return BlockType.CODE
+
+    # Quote
+    quotes = []
+    for line in lines:
+        if re.search(r"^>", line):
+            quotes.append(line)
+    if len(lines) == len(quotes):
+        return BlockType.QUOTE
+
+    # Unordered list
+    uls = []
+    for line in lines:
+        if re.search(r"^(\*|-)\s", line):
+            uls.append(line)
+    if len(lines) == len(uls):
+        return BlockType.UNORDERED_LIST
+
+    # Ordered list
+    ols = []
+    for line in lines:
+        if re.search(r"^\d*\.\s", line):
+            ols.append(line)
+    if len(lines) == len(ols):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -53,6 +53,114 @@ But this is just some text.
         hope = ["# This is a heading", "But this is just some text."]
 
         self.assertListEqual(result, hope)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+
+    def test_heading_1(self):
+        block = "# Hi"
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.HEADING.value)
+
+    def test_heading_2(self):
+        block = "## Hi"
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.HEADING.value)
+
+    def test_heading_3(self):
+        block = "### Hi"
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.HEADING.value)
+
+    def test_heading_4(self):
+        block = "#### Hi"
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.HEADING.value)
+
+    def test_heading_5(self):
+        block = "##### Hi"
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.HEADING.value)
+
+    def test_heading_6(self):
+        block = "###### Hi"
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.HEADING.value)
+
+    def test_code_small(self):
+        block = "```Hi```"
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.CODE.value)
+
+    def test_code_large(self):
+        block = """```
+        Hi
+        ```"""
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.CODE.value)
+
+    def test_code_large_multiline(self):
+        block = """```
+        Hi
+        this is
+        code
+        ```"""
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.CODE.value)
+
+    def test_code_large_multiline_plus_empty_lines(self):
+        block = """```
+        Hi
+        this is
+        code
+
+        def test():
+            print("hello")
+            return 0
+
+        if __name__ == "__main__":
+            test()
+
+        ```"""
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.CODE.value)
+
+    def test_quote_small(self):
+        block = """>Hi"""
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.QUOTE.value)
+
+    def test_quote_large(self):
+        block = """> Hi
+> testing
+>multiline
+> Done."""
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.QUOTE.value)
+
+    def test_ordered_list_small(self):
+        block = """1. Hi"""
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.ORDERED_LIST.value)
+
+    def test_ordered_list_large(self):
+        block = """1. Hi
+2. Test
+4. Hello """
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.ORDERED_LIST.value)
+
+    def test_unordered_list_small(self):
+        block = """- Hi"""
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.UNORDERED_LIST.value)
+
+    def test_unordered_list_large(self):
+        block = """* Hi
+- Test
+- Hello """
+        result = block_to_block_type(block)
+        self.assertEqual(result.value, BlockType.UNORDERED_LIST.value)
 
 
 if __name__ == "__main__":
